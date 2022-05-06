02768943ba2b92fbd741a8655a3dbab9aaaab257
m.alizadeh <alizadee.maedeh@gmail.com>
2022-05-06 17:39:20 +0430

    Responsive for mobile, Updated dropdownMenu In JS

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 const dropBtn = document.querySelectorAll('.dropdown-btn');
+
 dropBtn.forEach( item => item.addEventListener('click' , openDropMenu) );
 
 function openDropMenu(event){
     event.preventDefault();
+    // start by closing every dropdown
+    closeOpenDropdown();
+    // Set dropdown menu icon 
     this.classList.toggle("active");
-    const dropMenu = this.nextElementSibling;
-    if (dropMenu.classList.contains('show-dropmenu')) {
-        dropMenu.classList.remove('show-dropmenu');
-        this.style.fontWeight = "normal";
+    const dropItem = this.nextElementSibling;
+    // open the button was clicked
+    if (dropItem.classList.contains('show-dropmenu')) {
+        dropItem.classList.remove('show-dropmenu');
     } else {
-        dropMenu.classList.add('show-dropmenu');
-        this.style.fontWeight = "700";
+        dropItem.classList.add('show-dropmenu' );
     };
+    closeOpenDropdownIfClickOutsideElement(this , event);
+}
+
+
+// if a button is clicked and another dropdownMenu is open, it closes first
+function closeOpenDropdown() {
+    const dropMenu = document.querySelectorAll('.dropdown-menu');
+    dropMenu.forEach( item => {
+        item.classList.toggle('show-dropmenu' , false)
+
+    });
 }
